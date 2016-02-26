a1ed40b26631db19ddbefc6b964f1dc4f0860401
Ellis Berner <eberner@gmail.com>
2016-02-25 22:58:43 -0800

    clean up JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,16 +17,6 @@ $(document).ready(function() {
     return false;
   });
 
-  $('a[href="#contact"]').on("click", function() {
-    $('html,body').animate({ scrollTop:$('#contact').offset().top - 76 }, 750);
-    return false;
-  });
-
-  $('a[href="#map"]').on("click", function() {
-    $('html,body').animate({ scrollTop:$('#map').offset().top - 77 }, 750);
-    return false;
-  });
-
   $('#photo-gallery').masonry({
     columnWidth: 0,
     itemSelector: '.item',
@@ -37,17 +27,14 @@ $(document).ready(function() {
     type: 'image',
     closeBtnInside: true,
     removalDelay: 300,
-
-    //mainClass: 'mfp-fade',
     gallery: {
       enabled: true,
-      preload: [0,2], // read about this option in next Lazy-loading section
+      preload: [0,2],
       navigateByImgClick: true,
       arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
       closeMarkup: '<button title="%title%" class="mfp-close"><i class="mfp-close-icn">&times;</i></button>',
-      tPrev: 'Previous (Left arrow key)', // title for left button
-      tNext: 'Next (Right arrow key)', // title for right button
-      //tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+      tPrev: 'Previous (Left arrow key)',
+      tNext: 'Next (Right arrow key)',
     }
   });
 
